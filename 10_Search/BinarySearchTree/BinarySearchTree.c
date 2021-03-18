e44f6bfef01ce4487fb40b080fed1dce3d61b601
leeminju531 <leeminju531@github.com>
2021-03-18 18:43:35 +0900

    binary Search Tree

--- 10_Search/BinarySearchTree/BinarySearchTree.c
+++ 10_Search/BinarySearchTree/BinarySearchTree.c
@@ -1,6 +1,7 @@
 #include "BinarySearchTree.h"
 #include "BinaryTree3.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void BSTMakeAndInit(BTreeNode** pRoot)
 {
@@ -60,4 +61,98 @@ BTreeNode* BSTSearch(BTreeNode* bst, BSTData target)
 			cNode = GetRightSubTree(cNode);
 	}
 	return NULL;
+}
+
+BTreeNode* BSTRemove(BTreeNode** pRoot, BSTData target)
+{
+	BTreeNode* pVRoot = MakeBTreeNode(); // virtual root node
+	BTreeNode* pNode = pVRoot; // parent node;
+	BTreeNode* cNode = *pRoot; // current node;
+	BTreeNode* dNode; // delete node;
+
+	ChangeRightSubTree(pVRoot,*pRoot);
+
+	// searching target node
+	while(cNode != NULL && GetData(cNode) != target)
+	{
+		pNode = cNode;
+
+		if(target < GetData(cNode))
+			cNode = GetLeftSubTree(cNode);
+		else
+			cNode = GetRightSubTree(cNode);
+
+	}
+	if(cNode == NULL) // target data not exist in BST 
+		return NULL;
+
+	dNode = cNode;
+
+	// case 1 : delete node is terminal node.
+	if(GetLeftSubTree(dNode) == NULL && GetRightSubTree(dNode) == NULL)
+	{
+		if(GetLeftSubTree(pNode) == dNode)
+			RemoveLeftSubTree(pNode);
+		else
+			RemoveRightSubTree(pNode);
+	}
+	// case 2 : delete node has one child node.
+	else if(GetLeftSubTree(dNode) == NULL || GetRightSubTree(dNode) == NULL)
+	{
+		BTreeNode* dcNode; // pointer child node of (target Node will be deleted)
+
+		if(GetLeftSubTree(dNode) != NULL)
+			dcNode = GetLeftSubTree(dNode);
+		else
+			dcNode = GetRightSubTree(dNode);
+
+		if(GetLeftSubTree(pNode) == dNode)
+			ChangeLeftSubTree(pNode,dcNode);
+		else
+			ChangeRightSubTree(pNode,dcNode);
+	}
+	// case 3 : delete node has two child node.
+	// -> can replace most larget value of left subtree of delNode 
+	//    or most small value of right subtree of delNode.
+	// let's use second choice.(regardless of first case)
+	else
+	{
+		BTreeNode* mNode = GetRightSubTree(dNode); //  node
+		BTreeNode* mpNode = dNode; 
+		int delData;
+
+		// search replacing node
+		while(GetLeftSubTree(mNode) != NULL)
+		{
+			mpNode = mNode;
+			mNode = GetLeftSubTree(mNode);
+		}
+		delData = GetData(dNode);
+		SetData(dNode,GetData(mNode));
+
+		if(GetLeftSubTree(mpNode) == mNode)
+			ChangeLeftSubTree(mpNode,GetRightSubTree(mNode));
+		else
+			ChangeRightSubTree(mNode,GetRightSubTree(mNode));
+
+		dNode = mNode;
+		SetData(dNode,delData);
+	}
+
+	// deleted node is root node case -> 
+	if(GetRightSubTree(pVRoot) != *pRoot)
+		*pRoot = GetRightSubTree(pVRoot);
+
+
+	free(pVRoot); // virtual root free
+	return dNode;
+}
+
+void ShowIntData(int data)
+{
+	printf("%d ",data);
+}
+void BSTShowAll(BTreeNode* bst)
+{
+	InorderTraverse(bst,ShowIntData);
 }
--- 10_Search/BinarySearchTree/main.c
+++ 10_Search/BinarySearchTree/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "BinarySearchTree.h"
 
 int main(void)
@@ -16,29 +17,38 @@ int main(void)
 	BSTInsert(&bstRoot,3);
 	BSTInsert(&bstRoot,5);
 
-	sNode = BSTSearch(bstRoot,1);
-	if(sNode == NULL)
-		printf("searching fail\n");
-	else
-		printf("key value : %d\n",BSTGetNodeData(sNode));
+	BSTShowAll(bstRoot);printf("\n");
+	sNode = BSTRemove(&bstRoot,3);
+	free(sNode);
 
-	sNode = BSTSearch(bstRoot,4);
-	if(sNode == NULL)
-		printf("searching fail\n");
-	else
-		printf("key value : %d\n",BSTGetNodeData(sNode));	
+	BSTShowAll(bstRoot);printf("\n");
+	sNode = BSTRemove(&bstRoot,1);
+	free(sNode);
 
-	sNode = BSTSearch(bstRoot,6);
-	if(sNode == NULL)
-		printf("searching fail\n");
-	else
-		printf("key value : %d\n",BSTGetNodeData(sNode));
+	BSTShowAll(bstRoot);printf("\n");
+	// sNode = BSTSearch(bstRoot,1);
+	// if(sNode == NULL)
+	// 	printf("searching fail\n");
+	// else
+	// 	printf("key value : %d\n",BSTGetNodeData(sNode));
 
-	sNode = BSTSearch(bstRoot,7);
-	if(sNode == NULL)
-		printf("searching fail\n");
-	else
-		printf("key value : %d\n",BSTGetNodeData(sNode));
+	// sNode = BSTSearch(bstRoot,4);
+	// if(sNode == NULL)
+	// 	printf("searching fail\n");
+	// else
+	// 	printf("key value : %d\n",BSTGetNodeData(sNode));	
+
+	// sNode = BSTSearch(bstRoot,6);
+	// if(sNode == NULL)
+	// 	printf("searching fail\n");
+	// else
+	// 	printf("key value : %d\n",BSTGetNodeData(sNode));
+
+	// sNode = BSTSearch(bstRoot,7);
+	// if(sNode == NULL)
+	// 	printf("searching fail\n");
+	// else
+	// 	printf("key value : %d\n",BSTGetNodeData(sNode));
 
 	return 0;
 }
